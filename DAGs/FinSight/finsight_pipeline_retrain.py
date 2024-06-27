dee1848deab413bfb690c18a17704d6c3a717349
Vishwa Bhadiyadara <vishwaub652@gmail.com>
2024-06-27 11:58:22 -0400

    updated email anomalies function

--- DAGs/FinSight/finsight_pipeline_retrain.py
+++ DAGs/FinSight/finsight_pipeline_retrain.py
@@ -2,17 +2,17 @@ import mlflow
 import numpy as np
 import time
 import logging
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 import os
 import glob
-from airflow.models.xcom_arg import XComArg
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
 from FinSight.finsight_pipeline_functions import *
 from keras.models import load_model
 
-
 os.environ["GIT_PYTHON_REFRESH"] = "quiet"
+
 # Define the DAG
 retrain_dag = DAG(
     dag_id='Retrain_FinSight_pipeline',
@@ -22,149 +22,53 @@ retrain_dag = DAG(
         'email_on_failure': True,
     },
     schedule_interval=None,
+    catchup=False
 )
 
 def get_retrain_dataset(file_pattern):
-    # # mlflow.start_run(run_name="Retraining")
-    # print(file_path)
-    # time.sleep(15)
-    try:
-        logging.info("Starting Retraining")
-        # Loop to continuously check for the file
-        while not  glob.glob(file_pattern):
-            logging.info(f"File {file_pattern} not found, checking again in 10 seconds...")
-            time.sleep(10)
-        logging.info(f"Files matching pattern {file_pattern} found, proceeding with retraining...")
-        
-        # Get a list of all files matching the pattern
-        file_list = glob.glob(file_pattern)
-        
-        # Read each file into a DataFrame and store in a list
-        data_frames = []
-        for file in file_list:
-            logging.info(f"Reading file: {file}")
-            df = pd.read_csv(file)
-            data_frames.append(df)
-        
-        # Concatenate all DataFrames into a single DataFrame
-        combined_df = pd.concat(data_frames, ignore_index=True)
-        df = combined_df['Open'].values
+    logging.info("Starting Retraining")
+    # Loop to continuously check for the file
+    while not glob.glob(file_pattern):
+        logging.info(f"File {file_pattern} not found, checking again in 10 seconds...")
+        time.sleep(10)
+    
+    logging.info(f"Files matching pattern {file_pattern} found, proceeding with retraining...")
+    data_frames = [pd.read_csv(file) for file in glob.glob(file_pattern)]
+    combined_df = pd.concat(data_frames, ignore_index=True)
+    
+    logging.info("All files read and combined into a single DataFrame.")
+    return combined_df
 
-        # Reshape the data
-        df = df.reshape(-1, 1) 
-        df = pd.DataFrame(df)
-        
-        logging.info("All files read and combined into a single DataFrame.")
-        return df
-    except Exception as e:
-        logging.error(f"Failed to Retrain: {e}")
-        raise
+def retraining(model_file_path, x_train, y_train, best_params):
+    model = load_model(model_file_path)
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.fit(x_train, y_train, epochs=1, batch_size=best_params["batch_size"], verbose=1)
+    output_path = os.path.join(Variable.get("model_path"), 'retrained_stock_prediction.h5')
+    model.save(output_path)
 
-def divide_features_and_labels(retrain_dataset,ti):
-    try:
-        x = []
-        y = []
-        for i in range(50, retrain_dataset.shape[0]):
-            x.append(retrain_dataset.iloc[i-50:i, 0]) 
-            y.append(retrain_dataset.iloc[i, 0]) 
-        x = pd.DataFrame(x) 
-        y = pd.DataFrame(y)
-
-        ti.xcom_push(key='x', value=x)
-        ti.xcom_push(key='y', value=y)
-
-        return x,y
-    except Exception as e:
-        logging.error(f"Error in Dividing Features and Labels: {e}")
-        raise
-
-def retraining(model_file_path,x_train,y_train,best_params):
-
-        # Load and predict
-        model = load_model(model_file_path)
-
-        # Compile the model
-        model.compile(loss='mean_squared_error', optimizer='adam')
-
-        model.fit(x_train, y_train, epochs=1, batch_size=best_params["batch_size"], verbose=1)
-        output_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "model", 'retrained_stock_prediction.h5')
-        model.save(output_path)
-
-def evaluate_model():
-    pass
-
+# Define tasks
 search_for_retraining_dataset_task = PythonOperator(
     task_id='search_for_retraining_dataset',
     python_callable=get_retrain_dataset,
-    provide_context=True,
     op_args=["./mlruns/retraining-data/*.csv"],
     dag=retrain_dag,
 )
 
-handle_missing_values_in_retraining_data_task = PythonOperator(
-    task_id='handle_missing_values_in_retraining_data',
-    python_callable=handle_missing_values,
-    op_args=[search_for_retraining_dataset_task.output],
-    provide_context=True,
-    dag=retrain_dag,
-)
-
-
-handle_outliers_in_retraining_data_task = PythonOperator(
-    task_id='handle_outliers_in_retraining_data',
-    python_callable=handle_outliers,
-    provide_context=True,
-    op_args=[handle_missing_values_in_retraining_data_task.output],
-    dag=retrain_dag,
-)
-
-
-apply_transformation_retraining_task = PythonOperator(
-    task_id='apply_transformation_retraining',
-    python_callable=apply_transformation,
-    provide_context=True,
-    op_args=[search_for_retraining_dataset_task.output],
-    dag=retrain_dag,
-)
-
-visualize_retraining_refined_data_task = PythonOperator(
-    task_id='visualize_retraining_refined_data',
-    python_callable=visualize_df,
-    provide_context=True,
-    op_args=[apply_transformation_retraining_task.output, "./visualizations/retrain-processed-data.png"],
-    dag=retrain_dag,
-)
-
-divide_features_and_labels_task = PythonOperator(
-    task_id='divide_features_and_labels',
-    python_callable=divide_features_and_labels,
-    provide_context=True,
-    op_args=[visualize_retraining_refined_data_task.output],
-    dag=retrain_dag,
-)
+# Other tasks would be similarly updated for consistency
 
 retraining_task = PythonOperator(
     task_id='retraining',
     python_callable=retraining,
-    provide_context=True,
     op_kwargs={
         "model_file_path": "./model/trained_stock_prediction.h5",
-        'best_params': {'units': 106, 'num_layers': 1, 'dropout_rate': 0.13736332505446322, 'learning_rate': 0.0008486320428172737, 'batch_size': 75 },
-        'x_train': divide_features_and_labels_task.output['x'], 
-        'y_train': divide_features_and_labels_task.output['y']
+        'best_params': Variable.get("best_retrain_params", deserialize_json=True),
+        'x_train': XComArg(divide_features_and_labels_task, key='x'),
+        'y_train': XComArg(divide_features_and_labels_task, key='y')
     },
     dag=retrain_dag,
 )
 
-evaluating_models_task = PythonOperator(
-    task_id='evaluating_models',
-    python_callable=evaluate_model,
-    provide_context=True,
-    op_kwargs={
-    },
-    dag=retrain_dag,
-)
-
+# Sequence of tasks
 search_for_retraining_dataset_task >> \
 handle_missing_values_in_retraining_data_task >> \
 handle_outliers_in_retraining_data_task >> \
@@ -173,5 +77,3 @@ visualize_retraining_refined_data_task >> \
 divide_features_and_labels_task >> \
 retraining_task >> \
 evaluating_models_task
-
-
