0efe3ab6676248fbf87e568bff8ef07bdb6eed18
Vishwa Bhadiyadara <vishwaub652@gmail.com>
2024-06-27 13:07:19 -0400

    Add funtionalities for anomalies in taks and funtions and mordified retrain

--- DAGs/FinSight/finsight_pipeline_taks.py
+++ DAGs/FinSight/finsight_pipeline_taks.py
@@ -1,3 +1,4 @@
+from FinSight.finsight_pipeline_functions import calculate_and_display_anomalies, check_and_email_anomalies
 from airflow.operators.python_operator import PythonOperator#,# ExternalTaskMarker
 from airflow.sensors.external_task_sensor import ExternalTaskSensor
 from airflow import DAG
@@ -5,6 +6,9 @@ from datetime import datetime
 from airflow import configuration as conf
 import sys, os
 import functools
+# from airflow.models import Variable
+# import smtplib
+# from email.mime.text import MIMEText
 
 # Add the directory containing finsight_pipeline_functions.py to the Python path
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
@@ -42,10 +46,9 @@ download_and_uploadToDVCBucket_task = PythonOperator(
     provide_context=True,
 )
 
-
 visualize_raw_data_task = PythonOperator(
     task_id='visualize_data',
-    op_args=[download_and_uploadToDVCBucket_task.output, "./visualizations/raw-data-viz.png"],
+    op_args=[download_and_uploadToDVCBucket_task.output, "/opt/airflow/visualizations/data1-viz.png"],
     python_callable=visualize_raw_data,
     provide_context=True,
 )
@@ -72,6 +75,20 @@ handle_missing_values_in_training_data_task = PythonOperator(
     dag=dag,
 )
 
+# handle_missing_values_in_evaluation_data_task = PythonOperator(
+#     task_id='handle_missing_values_in_evaluation_data',
+#     python_callable=functools.partial(handle_missing_values_task, split='eval'),
+#     provide_context=True,
+#     dag=dag,
+# )
+
+# handle_missing_values_in_test_data_task = PythonOperator(
+#     task_id='handle_missing_values_in_test_data',
+#     python_callable=functools.partial(handle_missing_values_task, split='test'),
+#     provide_context=True,
+#     dag=dag,
+# )
+
 handle_outliers_in_training_data_task = PythonOperator(
     task_id='handle_outliers_in_training_data',
     python_callable=handle_outliers,
@@ -81,6 +98,25 @@ handle_outliers_in_training_data_task = PythonOperator(
 )
 handle_missing_values_in_training_data_task.set_downstream(handle_outliers_in_training_data_task)
 
+# handle_outliers_in_evaluation_data_task = PythonOperator(
+#     task_id='handle_outliers_in_evaluation_data',
+#     python_callable=handle_outliers,
+#     provide_context=True,
+#     op_args=[handle_missing_values_in_evaluation_data_task.output],
+#     dag=dag,
+# )
+# handle_missing_values_in_evaluation_data_task.set_downstream(handle_outliers_in_evaluation_data_task)
+
+# handle_outliers_in_test_data_task = PythonOperator(
+#     task_id='handle_outliers_in_test_data',
+#     python_callable=handle_outliers,
+#     provide_context=True,
+#     op_args=[handle_missing_values_in_test_data_task.output],
+#     dag=dag,
+# )
+# handle_missing_values_in_test_data_task.set_downstream(handle_outliers_in_test_data_task)
+
+##
 generate_scheme_and_stats_training_task = PythonOperator(
     task_id='generate_scheme_and_stats_training',
     python_callable=generate_scheme_and_stats,
@@ -90,11 +126,29 @@ generate_scheme_and_stats_training_task = PythonOperator(
 )
 handle_outliers_in_training_data_task.set_downstream(generate_scheme_and_stats_training_task)
 
+# generate_scheme_and_stats_eval_task = PythonOperator(
+#     task_id='generate_scheme_and_stats_eval',
+#     python_callable=generate_scheme_and_stats,
+#     provide_context=True,
+#     op_args=[handle_outliers_in_evaluation_data_task.output],
+#     dag=dag,
+# )
+# handle_outliers_in_evaluation_data_task.set_downstream(generate_scheme_and_stats_eval_task)
+
+# generate_scheme_and_stats_test_task = PythonOperator(
+#     task_id='generate_scheme_and_stats_test',
+#     python_callable=generate_scheme_and_stats,
+#     provide_context=True,
+#     op_args=[handle_outliers_in_test_data_task.output],
+#     dag=dag,
+# )
+# handle_outliers_in_test_data_task.set_downstream(generate_scheme_and_stats_test_task)
+
 def calculate_and_display_anomalies_task(ti, split):
     df = ti.xcom_pull(task_ids='divide_train_eval_test_splits', key=split)
     scheme = ti.xcom_pull(task_ids='generate_scheme_and_stats_training', key="schema")
     stats = ti.xcom_pull(task_ids='generate_scheme_and_stats_training', key="stats")
-    handled_df = calculate_and_display_anomalies(df, scheme, stats, ti)
+    handled_df = calculate_and_display_anomalies(df, scheme, stats)
     return handled_df
 
 
@@ -106,6 +160,38 @@ calculate_and_display_anomalies_eval_task = PythonOperator(
 )
 generate_scheme_and_stats_training_task.set_downstream(calculate_and_display_anomalies_eval_task)
 
+send_anomalies_notification_task = PythonOperator(
+    task_id='send_anomalies_notification',
+    python_callable=check_and_email_anomalies,
+    op_kwargs={'anomalies': "{{ ti.xcom_pull(task_ids='calculate_and_display_anomalies_eval', key='return_value') }}"},
+    dag=dag,
+)
+
+
+# Setting dependencies
+
+calculate_and_display_anomalies_eval_task >> send_anomalies_notification_task
+
+
+# calculate_and_display_anomalies_eval_task = PythonOperator(
+#     task_id='calculate_and_display_anomalies_eval',
+#     python_callable=calculate_and_display_anomalies,
+#     provide_context=True,
+#     op_args=[generate_scheme_and_stats_eval_task.output],
+#     dag=dag,
+# )
+# generate_scheme_and_stats_eval_task.set_downstream(calculate_and_display_anomalies_eval_task)
+
+# calculate_and_display_anomalies_test_task = PythonOperator(
+#     task_id='calculate_and_display_anomalies_test',
+#     python_callable=calculate_and_display_anomalies,
+#     op_args=[generate_scheme_and_stats_test_task.output],
+#     provide_context=True,
+#     dag=dag,
+# )
+# generate_scheme_and_stats_test_task.set_downstream(calculate_and_display_anomalies_test_task)
+
+##
 
 apply_transformation_training_task = PythonOperator(
     task_id='apply_transformation_training',
@@ -123,7 +209,6 @@ apply_transformation_eval_task = PythonOperator(
     op_args=[divide_train_eval_test_splits_task.output["eval"]],
     dag=dag,
 )
-
 apply_transformation_training_task.set_downstream(apply_transformation_eval_task)
 
 apply_transformation_test_task = PythonOperator(
@@ -133,7 +218,6 @@ apply_transformation_test_task = PythonOperator(
     op_args=[divide_train_eval_test_splits_task.output["test"]],
     dag=dag,
 )
-
 apply_transformation_training_task.set_downstream(apply_transformation_test_task)
 
 
@@ -168,21 +252,20 @@ hyper_parameter_tuning_task = PythonOperator(
     task_id='hyper_parameter_tuning',
     python_callable=hyper_parameter_tuning,
     provide_context=True,
+    # op_args=[divide_features_and_labels_task.output],
     op_kwargs={'x':  divide_features_and_labels_task.output['x'], 'y':  divide_features_and_labels_task.output['y']},
     dag=dag,
 )
-
 
 training_task = PythonOperator(
     task_id='training',
     python_callable=training,
     provide_context=True,
     op_args=[hyper_parameter_tuning_task.output],
-    op_kwargs={'x': divide_features_and_labels_task.output['x'], 'y': divide_features_and_labels_task.output['y']},
+    op_kwargs={'x':  divide_features_and_labels_task.output['x'], 'y':  divide_features_and_labels_task.output['y']},
     dag=dag,
 )
 hyper_parameter_tuning_task.set_downstream(training_task)
-
 
 
 load_and_predict_task = PythonOperator(
@@ -214,4 +297,4 @@ evaluate_and_visualize_task = PythonOperator(
 
 
 # Set the task dependencies
-download_and_uploadToDVCBucket_task >> visualize_raw_data_task >> divide_train_eval_test_splits_task >> handle_missing_values_in_training_data_task 
+download_and_uploadToDVCBucket_task >> visualize_raw_data_task >> divide_train_eval_test_splits_task >> handle_missing_values_in_training_data_task
